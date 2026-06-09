6fa85a606d5bfec84bd1c24930dd1ea38d2d3fd3
Hoàng Ngọc Lâm <hoangngoclamlc@gmail.com>
2026-06-09 16:35:04 +0700

    Update ketnoi.js

--- PhieuGiaoViec/ketnoi.js
+++ PhieuGiaoViec/ketnoi.js
@@ -7,9 +7,9 @@ var PGV_TEN_TRUONG = "Trường TH&THCS Hợp Thành";
 var PGV_TEN_TRUONG_UP = "TRƯỜNG TH&THCS HỢP THÀNH";
 var PGV_TEN_PM = "Quản lý Nhiệm vụ";
 var PGV_TEN_PM_UP = "QUẢN LÝ CÔNG VIỆC";
-var PGV_PHIEN_BAN = "V2.3.2026";
+var PGV_PHIEN_BAN = "V17.6.2026";
 var PGV_TAC_GIA = "HOÀNG NGỌC LÂM";
-var PGV_GOOGLE_CLIENT_ID = "808915800369-47d1eeka1a6qecttke9mkdndfco7dlls.apps.googleusercontent.com";
+var PGV_GOOGLE_CLIENT_ID = "1097384743947-1jdc5rhhmbu0s9jp5vgt814g4f4id7lu.apps.googleusercontent.com";
 var PGV_MO_TA = "Hệ thống quản lý hiệu quả công việc nội bộ - " + PGV_TEN_TRUONG;
 
 // ==================== BIẾN TOÀN CỤC HỆ THỐNG ====================
