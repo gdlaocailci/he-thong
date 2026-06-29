10e54138d4a348518d420396b741fdecec0d5de5
Hoàng Ngọc Lâm <hoangngoclamlc@gmail.com>
2026-06-29 11:40:38 +0700

    Update ketnoi.js

--- QuanLyHoSo/ketnoi.js
+++ QuanLyHoSo/ketnoi.js
@@ -5,16 +5,19 @@
  */
 
 const KETNOI = {
-    // 3. Đường dẫn máy chủ xử lý dữ liệu (Google Apps Script API)
+    // 1. Đường dẫn máy chủ xử lý dữ liệu (Google Apps Script API)
     WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxJEriNwHp3twwlVk_yqViktUo96XOj71ciMdNsNY8TSuH_76t7gPt48DGnWU34gWVdeQ/exec",
 
-    // 4. Mã định danh ứng dụng Google (OAuth 2.0 Client ID)
+    // 2. Mã định danh ứng dụng Google (OAuth 2.0 Client ID)
     CLIENT_ID: "1097384743947-1jdc5rhhmbu0s9jp5vgt814g4f4id7lu.apps.googleusercontent.com",
-    // 1. Thông tin định danh cơ quan/đơn vị
+    // 3. Thông tin định danh cơ quan/đơn vị
     TEN_DINH_DANH: "Hợp Thành OS",
     TIEU_DE_TRANG: "Cổng thông tin Quản lý hồ sơ - Trường TH&THCS Hợp Thành",
     
-    // 2. Tài nguyên hình ảnh đại diện (Logo & Giao diện)
+    // Bổ sung liên kết YouTube Hướng dẫn
+    LINK_YOUTUBE: "https://www.youtube.com/channel/UCdfiTWwv78VITvzlgxnv02Q",
+    
+    // 4. Tài nguyên hình ảnh đại diện (Logo & Giao diện)
     LINK_LOGO: "https://i.ibb.co/6R8Y9DNq/logo-l-a-ch-n6.png",
     LINK_HINH_NEN: "https://i.ibb.co/h1CBtJNW/Hinh-Nen-Ruong-Bac-Thang-15.jpg",
     LINK_AVATAR_NAM: "https://lh3.googleusercontent.com/d/13OnSd7t73KO7GP6NB7FjkdpsO6vfH-dp",
