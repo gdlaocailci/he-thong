3e3384a9c926fdc18b5e8bbe996cbe47fbacd176
Hoàng Ngọc Lâm <hoangngoclamlc@gmail.com>
2026-07-24 06:38:09 +0700

    Update ketnoi.js

--- SoKiemTraHopThanh/ketnoi.js
+++ SoKiemTraHopThanh/ketnoi.js
@@ -19,7 +19,7 @@ const SKT_TAC_GIA_UP = "THIẾT KẾ VÀ PHÁT TRIỂN\nHOÀNG NGỌC LÂM";
 const MA_TRAN_UI_CLIENT = {
   "DANG_NHAP": {
     TEN_TRUONG: "HÃY CHỌN ĐƠN VỊ",
-    TEN_TRUONG_UP: "",
+    TEN_TRUONG_UP: "HÃY CHỌN ĐƠN VỊ",
     LOGO_URL: "https://i.ibb.co/p6G2MsnB/Chat-GPT-Image-07-54-46-4-thg-7-2026.png"
   },
   "TRUONG_1": {
