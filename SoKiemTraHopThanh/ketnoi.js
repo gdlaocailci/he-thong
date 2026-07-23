f2d3bd4373dc768a8bf38042add0e51f799e1670
Hoàng Ngọc Lâm <hoangngoclamlc@gmail.com>
2026-07-24 06:40:05 +0700

    Refactor UI configuration and connection logic

--- SoKiemTraHopThanh/ketnoi.js
+++ SoKiemTraHopThanh/ketnoi.js
@@ -18,7 +18,7 @@ const SKT_TAC_GIA_UP = "THIẾT KẾ VÀ PHÁT TRIỂN\nHOÀNG NGỌC LÂM";
 /* KHỐI MA TRẬN CẤU HÌNH GIAO DIỆN TRÌNH DUYỆT */
 const MA_TRAN_UI_CLIENT = {
   "DANG_NHAP": {
-    TEN_TRUONG: "HÃY CHỌN ĐƠN VỊ",
+    TEN_TRUONG: "THẦY CÔ",
     TEN_TRUONG_UP: "HÃY CHỌN ĐƠN VỊ",
     LOGO_URL: "https://i.ibb.co/p6G2MsnB/Chat-GPT-Image-07-54-46-4-thg-7-2026.png"
   },
