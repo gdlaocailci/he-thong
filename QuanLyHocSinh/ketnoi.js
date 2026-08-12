345e49ceb666f4bec028e742e792309e4f4d4669
Hoàng Ngọc Lâm <hoangngoclamlc@gmail.com>
2026-08-12 13:31:30 +0700

    Update ketnoi.js

--- QuanLyHocSinh/ketnoi.js
+++ QuanLyHocSinh/ketnoi.js
@@ -1,6 +1,6 @@
 // CẤU HÌNH HỆ THỐNG GIAO TIẾP VÀ HIỂN THỊ (CLIENT)
 // ===========================================================================
-const HKS_CAU_HINH_WEBAPP_STR = "https://script.google.com/macros/s/AKfycbwY_LDKefb80LVnegm_IwoSWzvpAuZB7cLqefApUW4eDiYG3Qc4JYqjLgoJT8SRKG9W/exec"; 
+const HKS_CAU_HINH_WEBAPP_STR = "https://script.google.com/macros/s/AKfycby4qB7Zj_KxG8treAdSAQP48KeSnlS0qZRuAQ0Uf0AOTgy3fEfW2Lr2EWWT6ONJBrc3GA/exec"; 
 const HKS_CAU_HINH_CLIENT_ID_STR = "1097384743947-1jdc5rhhmbu0s9jp5vgt814g4f4id7lu.apps.googleusercontent.com";
 const HKS_CAU_HINH_LOGO_STR = "https://i.ibb.co/d0JNW94Z/logo-l-a-ch-n6.png";
 const HKS_CAU_HINH_TEN_DINH_DANH_STR = "Trường TH&THCS Hợp Thành";
