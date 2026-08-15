3ca5acac1535f9829ab9ff849dd37c2bb6745f10
Hoàng Ngọc Lâm <hoangngoclamlc@gmail.com>
2026-08-15 22:27:50 +0700

    Update KetNoi.js

--- ThoiKhoaBieu/KetNoi.js
+++ ThoiKhoaBieu/KetNoi.js
@@ -1,21 +1,19 @@
 const CAU_HINH_FRONTEND = {
-    // =========================================================================
-    // 1. LINK CỔNG GIAO TIẾP VỚI MÁY CHỦ GOOGLE APPS SCRIPT (Cực kỳ quan trọng)
-    // =========================================================================
     URL_API_MAY_CHU: 'https://script.google.com/macros/s/AKfycbz1IOFWbcOTfgPmnaSeD2uU0i7UWWsUSW3pHja87WzT3GOhtwsJz7YyNdY2eOHGU33s/exec',
-
-   TEN_DU_AN: 'CỔNG TRA CỨU THỜI KHÓA BIỂU - TRƯỜNG TIỂU HỌC HỢP THÀNH',
+    TEN_DU_AN: 'CỔNG TRA CỨU THỜI KHÓA BIỂU - TRƯỜNG TIỂU HỌC HỢP THÀNH',
     
     // Cấu hình chữ ký Footer hiển thị 2 dòng
     TIEU_DE_TAC_GIA: 'Thiết kế và phát triển',
     TAC_GIA_THIET_KE: 'Hoàng Ngọc Lâm',
 
-    // =========================================================================
-    // 3. LINK TÀI NGUYÊN (ICONS, LOGO) - Lấy từ SvgRepo.com
-    // =========================================================================
-    LINK_LOGO_TRANG_CHU: 'https://i.postimg.cc/Hn7bdGg5/Logo-truong-Photoroom.png',
+    LINK_LOGO_TRANG_CHU: 'https://www.svgrepo.com/show/339459/calendar.svg',
     LINK_ICON_BANG: 'https://www.svgrepo.com/show/521634/file-table.svg',
     LINK_ICON_DANG_TAI: 'https://www.svgrepo.com/show/295350/loading-process.svg',
     LINK_ICON_TRONG: 'https://www.svgrepo.com/show/334002/database-search.svg',
     LINK_ICON_LAM_MOI: 'https://www.svgrepo.com/show/521815/refresh.svg'
 };
+
+// =========================================================================
+// KHỐI 4: CẤU HÌNH XÁC THỰC (GOOGLE IDENTITY)
+// =========================================================================
+const SKT_GOOGLE_CLIENT_ID = "1097384743947-1jdc5rhhmbu0s9jp5vgt814g4f4id7lu.apps.googleusercontent.com";
