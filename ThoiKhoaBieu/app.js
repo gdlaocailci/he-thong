31a57c74703a3f16b9b1459767c6fb681b05da4b
Hoàng Ngọc Lâm <hoangngoclamlc@gmail.com>
2026-08-24 16:14:03 +0700

    Update app.js

--- ThoiKhoaBieu/app.js
+++ ThoiKhoaBieu/app.js
@@ -151,7 +151,7 @@ async function khoiTaoGiaoDien() {
                 theTrangThai.innerText = 'Hệ thống mở';
                 theTrangThai.className = 'font-bold text-green-700 text-base leading-tight inline-block mt-0.5';
             } else {
-                theTrangThai.innerText = 'Hệ thống đang bảo trì'; 
+                theTrangThai.innerText = 'Hệ thống đang nâng cấp/bảo trì'; 
                 theTrangThai.className = 'font-bold text-red-600 text-base leading-tight inline-block mt-0.5 reactbits-bap-benh';
             }
         }
