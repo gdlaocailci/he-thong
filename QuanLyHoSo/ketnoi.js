df577fc0cbc5f623a8723ced1941abc1c9768a7f
Hoàng Ngọc Lâm <hoangngoclamlc@gmail.com>
2026-09-16 05:54:45 +0700

    Update ketnoi.js

--- QuanLyHoSo/ketnoi.js
+++ QuanLyHoSo/ketnoi.js
@@ -14,7 +14,7 @@ const KETNOI = {
     LINK_YOUTUBE: "https://www.youtube.com/channel/UCdfiTWwv78VITvzlgxnv02Q",
     
     // 3. Đường dẫn máy chủ xử lý dữ liệu (Google Apps Script API)
-    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzoWr-UmfIKUn8FLtnT3wUHw6xEeUtQu0V7fOALceb0uXUAcBjufPU8lqHwVEyVgf8H/exec",
+    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxJEriNwHp3twwlVk_yqViktUo96XOj71ciMdNsNY8TSuH_76t7gPt48DGnWU34gWVdeQ/exec",
 
     // 4. Mã định danh ứng dụng Google (OAuth 2.0 Client ID)
     CLIENT_ID: "1097384743947-1jdc5rhhmbu0s9jp5vgt814g4f4id7lu.apps.googleusercontent.com"
